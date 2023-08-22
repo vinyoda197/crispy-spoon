1aefa18d60e03e9fa911120974a44371ccf39b00
Vincent Garcia <vincentbgarcia@gmail.com>
2023-08-22 18:05:30 +0800

    initial push

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+
+export default function About() {
+    return (
+        <>
+        <section className="w-full bg-stone-300">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+            <div className="text-gray-500 sm:text-lg dark:text-gray-400">
+                <h2 className="mb-4 text-center text-4xl tracking-tight font-bold text-gray-900">About</h2>
+            </div>
+        </div>
+        </section>
+        </>
+    )
+}
